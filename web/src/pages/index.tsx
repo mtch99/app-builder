64f731e797f498af2c2b79cfff2cf201a6631b5b
Maxime Tchagou <maximefxtchagou@yahoo.com>
2023-12-04 14:28:46 -0500

    refactor: Home view

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,36 +1,61 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import type { NextPage } from "next";
-import { Button, Col, Container, Row, Card, Navbar, Nav } from "react-bootstrap";
+import { Button, Col, Container, Row, Card, Navbar, Nav, ButtonGroup } from "react-bootstrap";
 
 export default function Home() {
   return (
     <Container style={{flex:1, margin:0}}>
       <Row>
-        <Navbar style={{backgroundColor: "transparent"}}>
-
-        </Navbar>
+        <TopBar/>
       </Row>
-      <Row>
+      <Row className='vh-100'>
         <Col style={{paddingLeft:0}} xxl={1}>
-          <Nav defaultActiveKey="/home" className="flex-lg-column flex-shrink-1" navbar={true}>
-            <Nav.Link href="/home">Active</Nav.Link>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-            <Nav.Link eventKey="disabled" disabled>
-              Disabled
-            </Nav.Link>
-          </Nav>
+          <SideBar/>
         </Col>
-        <Col style={{backgroundColor:"#FFF8F4", paddingRight:0}} className='flex-grow-1' xxl={true}>
-          <Container fluid={true}>
-            <Row className='vh-100 flex-grow-1'>
-
-            </Row>
-          </Container>
+        <Col style={{backgroundColor:"#FFF8F4", paddingRight:0}} xxl={true}>
+          <Body/>
         </Col>
       </Row>
     </Container>
   )
 
+}
+
+
+const TopBar = () => {
+  return(
+    <Navbar style={{backgroundColor: "transparent"}}>
+      <Container>
+        <Navbar.Brand>Casa Grecque Icon</Navbar.Brand>
+        <ButtonGroup>
+        </ButtonGroup>
+      </Container>
+    </Navbar>
+  )
+}
+
+
+const SideBar = () => {
+  return (
+    <Nav defaultActiveKey="/home" className="flex-lg-column flex-shrink-1" navbar={true}>
+      <Nav.Link href="/home">Active</Nav.Link>
+      <Nav.Link eventKey="link-1">Link</Nav.Link>
+      <Nav.Link eventKey="link-2">Link</Nav.Link>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link>
+    </Nav>
+  )
+}
+
+
+const Body = () => {
+  return (
+    <Container fluid={true}>
+      <Row>
+
+      </Row>
+    </Container>
+  )
 }

ce7cf0e5d35ee358b697ce172ba196884351d714
Maxime Tchagou <maximefxtchagou@yahoo.com>
2023-12-04 20:04:24 -0500

    refactor: Home page components

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,53 +4,53 @@ import type { NextPage } from "next";
 import { Button, Col, Container, Row, Card, Navbar, Nav, ButtonGroup } from "react-bootstrap";
 
 export default function Home() {
-  return (
-    <Container fluid={true} className='d-flex flex-column vh-100'>
-      <TopBar/>
-        <Row className='h-100'>
-          <Col style={{paddingLeft:0}} xxl={1}>
-            <SideBar/>
-          </Col>
-          <Col style={{backgroundColor:"#FFF8F4"}} xxl={true}>
-            <Body/>
-          </Col>
-        </Row>
-    </Container>
-  )
-
+	return (
+		<Container fluid={true} className='d-flex flex-column vh-100'>
+			<TopBar/>
+			<Row className='h-100'>
+				<SideBar/>
+				<Body/>
+			</Row>
+		</Container>
+	)
+		
 }
-
-
+	
+	
 const TopBar = () => {
-  return(
-    <Navbar>
-      <Container>
-        <Navbar.Brand style={{backgroundColor: "white"}}>Casa Grecque Icon</Navbar.Brand>
-        <ButtonGroup>
-        </ButtonGroup>
-      </Container>
-    </Navbar>
-  )
+	return(
+		<Navbar>
+			<Container>
+				<Navbar.Brand style={{backgroundColor: "white"}}>Casa Grecque Icon</Navbar.Brand>
+				<ButtonGroup>
+				</ButtonGroup>
+			</Container>
+		</Navbar>
+	)
 }
-
-
+		
+		
 const SideBar = () => {
-  return (
-    <Nav defaultActiveKey="/home" className="flex-lg-column flex-shrink-1" navbar={true}>
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav>
-  )
+	return (
+		<Col style={{paddingLeft:0}} xxl={1}>
+			<Nav defaultActiveKey="/home" className="flex-lg-column flex-shrink-1" navbar={true}>
+				<Nav.Link href="/home">Active</Nav.Link>
+				<Nav.Link eventKey="link-1">Link</Nav.Link>
+				<Nav.Link eventKey="link-2">Link</Nav.Link>
+				<Nav.Link eventKey="disabled" disabled>
+				Disabled
+				</Nav.Link>
+			</Nav>
+		</Col>
+	)
 }
-
-
+	
+	
 const Body = () => {
-  return (
-    <Container fluid={true} style={{backgroundColor:"#FCF2EA"}}>
-    </Container>
-  )
+	return (
+		<Col style={{backgroundColor:"#FFF8F4"}} xxl={true}>
+			<Container fluid={true} style={{backgroundColor:"#FCF2EA"}}>
+			</Container>
+		</Col>
+	)
 }

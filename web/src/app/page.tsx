0f1e4629eda909d9c82ad70a519b9f1a92d4020a
Maxime Tchagou <maximefxtchagou@yahoo.com>
2023-12-07 15:31:29 -0500

    added Achievements and Calendar to Home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,40 @@
+import { Button, Col, Container, Row, Card, Navbar, Nav, ButtonGroup } from "react-bootstrap";
+import Calendar from './components/Calendar';
+import { CSSProperties } from 'react';
+import Achievements from "./components/Achievements";
+
+
 export default function Home() {
   return (
-    <Body/>
+    <Body>
+		<BodyContent/>
+	</Body>
   )
 }
 
-import Link from 'next/link'
-import { Button, Col, Container, Row, Card, Navbar, Nav, ButtonGroup } from "react-bootstrap";
 
-
-const Body = () => {
+const Body = ({children} : {children: React.ReactNode}) => {
 	return (
 		<Col style={{backgroundColor:"transparent"}}>
 			{/** Background color here is surfaceContainer */}
-			<Container className='mb-3 h-100' style={{backgroundColor:"#F6ECE4", borderRadius:"1rem"}} fluid={true}>
+			<Container className='mb-3 h-100 p-4' style={{backgroundColor:"#F6ECE4", borderRadius:"1rem"}} fluid={true}>
 				{/** Background color here is surfaceContainerHighest  */}
-					<Row>
-            
-					</Row>
-				</Container>
+				{children}
+			</Container>
 		</Col>
 	)
 }
 
+
+const BodyContent = () => {
+	return(
+		<Row className='h-50'>
+			<Col className='h-100' style={{flex:3}}>
+				<Calendar/>
+			</Col>
+			<Col className='h-100' style={{flex: 1}}>
+				<Achievements/>
+			</Col>
+		</Row>
+	)
+}
